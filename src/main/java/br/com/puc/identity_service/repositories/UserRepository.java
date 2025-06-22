d3a206da83f9ef3b82683264b79bcb907740c275
Rony Amorim dos Reis <108841655+RonyAmorim@users.noreply.github.com>
2025-06-22 08:46:56 -0300

    update Keycloak configuration in docker-compose and add initial user data

--- src/main/java/br/com/puc/identity_service/repositories/UserRepository.java
+++ src/main/java/br/com/puc/identity_service/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package br.com.puc.identity_service.repositories;
 
 import br.com.puc.identity_service.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByKeycloakId(String keycloakId);
     Optional<UserEntity> findByUsername(String username);
